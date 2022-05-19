c4fdca2d4ee6b872a3ff07b2a49dadbeee104496
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2022-05-19 05:33:37 +0200

    Init

--- src/app/weatherDetails/weatherDetails.component.ts
+++ src/app/weatherDetails/weatherDetails.component.ts
@@ -9,8 +9,23 @@ import {Component, Input, OnInit} from '@angular/core';
 export class WeatherDetails implements OnInit {
   @Input() weatherData: data[];
 
+  town: string = '';
+  temperature: string = '';
+  wind: string = '';
+  humidity: string = '';
   ngOnInit() {
 
+  }
+
+  onTownChange(searchValue: string): void {
+    console.log(searchValue);
+    // let string = this.celsius;
+    // let num = parseInt(string) * 9 / 5 + 32;
+    // this.fahrenheit = num.toString();
+    const filteredResult = this.weatherData.find((e) => e.name == searchValue);
+    this.temperature=filteredResult.temperature;
+    this.humidity=filteredResult.humidity;
+    this.wind=filteredResult.wind;
   }
 }
 
